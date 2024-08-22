ebfa5fcfc3a909ea3fbba56a7fad2df153143236
Etativel <farhanmaulana4611@gmail.com>
2024-08-22 14:22:51 +0700

    finishing header and nav

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import "./styles/style.css"
 
-import image from "./images/burger.png"
+import hugeHeaderImage from "./images/burger.png"
+import headerMascotImage from "./images/mascot-nobg.png"
 
 const imageContainer = document.querySelector(".image-container")
 const headImage = document.createElement("img")
-headImage.src = image
+headImage.src = hugeHeaderImage
 headImage.classList.add('huge-image')
 imageContainer.appendChild(headImage)
+
+const mascotContainer = document.querySelector('.mascot-img-container')
+const mascotImage = document.createElement("img")
+mascotImage.src = headerMascotImage
+mascotImage.classList.add('mascot-img')
+mascotContainer.appendChild(mascotImage)
