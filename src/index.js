c49bb496b0c25e80427b7cccaa8d897e877fe09e
Etativel <farhanmaulana4611@gmail.com>
2024-08-23 20:25:45 +0700

    adding food image

--- src/index.js
+++ src/index.js
@@ -14,3 +14,47 @@ const mascotImage = document.createElement("img")
 mascotImage.src = headerMascotImage
 mascotImage.classList.add('mascot-img')
 mascotContainer.appendChild(mascotImage)
+
+// Best item image
+import bestNoodle from "./images/noodle-lomein.png"
+import bestChicken from "./images/chicken.png"
+import bestBurger from "./images/mayoburger.png"
+
+const bestItemList = [bestChicken, bestBurger,bestNoodle]
+const bestItemName = ['Fried Cicken', 'Burger', 'Noodle']
+
+// append to this element with class "best-item-1"
+const appendBestItem = function(){
+    for (let i = 0; i < bestItemList.length; i++){
+        const bestItemImageContainer = document.createElement("img")
+        bestItemImageContainer.src = bestItemList[i]
+        bestItemImageContainer.classList.add(`best-item-img-${i}`)
+        const currentItem = document.querySelector(`.best-item-${i+1}`)
+        currentItem.appendChild(bestItemImageContainer)
+        console.log(currentItem)
+
+        //This is the title and order link container
+        const bestItemTitleContainer = document.createElement("div") 
+        bestItemTitleContainer.classList.add(`b-title-container`)
+
+        //This is the order title
+        const bestItemTitle = document.createElement("p") 
+        bestItemTitle.textContent = bestItemName[i]
+        bestItemTitle.classList.add(`b-item-title`)
+
+        //This is the order link
+        const bestItemOrder = document.createElement("p")
+        bestItemOrder.classList.add(`b-item-order-link`)
+        bestItemOrder.textContent = "Order Now"
+
+        //Append the both p tag. The title and the link
+        bestItemTitleContainer.appendChild(bestItemTitle)
+        bestItemTitleContainer.appendChild(bestItemOrder)
+
+        //Append to the main container for current item
+        currentItem.appendChild(bestItemTitleContainer)
+    }
+}
+
+appendBestItem()
+
