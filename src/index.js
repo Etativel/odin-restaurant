fe101b18d10bbb187a62f52753f335e362205d19
Etativel <farhanmaulana4611@gmail.com>
2024-08-24 13:25:39 +0700

    adding menu function

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import "./styles/style.css"
 
 import hugeHeaderImage from "./images/burger.png"
 import headerMascotImage from "./images/mascot-nobg.png"
+import webIcon from "./images/cutlery.png"
+
+// favicon
+const iconContainer = document.querySelector('.favicon')
+iconContainer.href = webIcon
 
 const imageContainer = document.querySelector(".image-container")
 const headImage = document.createElement("img")
@@ -31,7 +36,7 @@ const appendBestItem = function(){
         bestItemImageContainer.classList.add(`best-item-img-${i}`)
         const currentItem = document.querySelector(`.best-item-${i+1}`)
         currentItem.appendChild(bestItemImageContainer)
-        console.log(currentItem)
+        // console.log(currentItem)
 
         //This is the title and order link container
         const bestItemTitleContainer = document.createElement("div") 
@@ -59,14 +64,124 @@ const appendBestItem = function(){
 
 appendBestItem()
 
-const menuImgs = document.querySelectorAll('.menu-img');
+// Append menu selection
 
-// Loop through each element and append an image
-menuImgs.forEach(menuImg => {
-    const menuImageElem = document.createElement('img');
-    menuImageElem.src = hugeHeaderImage;
-    menuImageElem.classList.add('image-1');
-    menuImg.appendChild(menuImageElem);
-});
+const menuSelectionContainer = document.querySelector('.menu-selection-container')
+
+const menuOptions = {
+    items: []
+  };
+
+const addMenu = (name, reviews, price, image) => {
+    menuOptions.items.push({
+        'name':name,
+        'reviews':reviews,
+        'price':price,
+        'image':image
+        
+    })
+}
+
+addMenu('Classic Beef Patty',' 241','$4.40',"./images/mayoburgerMenu.png")
+addMenu('Homemade Cookies',' 130','$2.20',"./images/cookies.png")
+addMenu('Lo Mein Noodles',' 223','$3.20',"./images/noodle-lomeinMenu.png")
+addMenu('Pad Thai Noodles',' 130','$4.20',"./images/pad-thai.png")
+addMenu('Pepperoni Pizza',' 112','$5.20',"./images/pizaa.png")
+addMenu('Beef Shawarma',' 140','$4.40',"./images/shawarma.png")
+addMenu('Soft Beef Tacos',' 111','$3.40',"./images/tacos.png")
+addMenu('Classic Cheese Burger',' 230','$4.40',"./images/burger.png")
+addMenu('Original Fried Chicken',' 211','$4.30',"./images/chicken.png")
 
 
+
+// need to make this dynamically
+// const imageAddresses = menuOptions.items.map(item => item['image']);
+import classicBeefPattyImg from './images/mayoburgerMenu.png';
+import homemadeCookiesImg from './images/cookies.png';
+import loMeinNoodlesImg from './images/noodle-lomeinMenu.png';
+import padThaiNoodlesImg from './images/pad-thai.png';
+import pepperoniPizzaImg from './images/pizaa.png';
+import beefShawarmaImg from './images/shawarma.png';
+import softBeefTacosImg from './images/tacos.png';
+import classicCheeseBurgerImg from './images/burger.png';
+import originalFriedChickenImg from './images/chicken.png';
+
+const imageList = [
+    classicBeefPattyImg,
+    homemadeCookiesImg,
+    loMeinNoodlesImg,
+    padThaiNoodlesImg,
+    pepperoniPizzaImg,
+    beefShawarmaImg,
+    softBeefTacosImg,
+    classicCheeseBurgerImg,
+    originalFriedChickenImg
+];
+
+const addMenuToDOM = () => {
+    for (let i = 0; i < menuOptions.items.length; i++){
+        // Create menu
+        const menu = document.createElement('div')
+        menu.classList.add('menu')
+
+            // Create menu img container
+            const menuImgContainer = document.createElement('div')
+            menuImgContainer.classList.add('menu-img')
+
+            //create menu img
+            const menuImg = document.createElement('img')
+            menuImg.classList.add('image')
+            menuImg.src = imageList[i]
+
+            // Create menu name
+            const menuName = document.createElement('div')
+            menuName.classList.add('menu-name')
+            menuName.textContent = menuOptions.items[i]['name']
+
+            //create menu-rate and star
+            const menuRate = document.createElement('div')
+            menuRate.classList.add('menu-rate')
+            for (let i = 0; i < 5; i++){
+                const star = document.createElement('span')
+                star.classList.add('fa', 'fa-star', 'star-checked')
+
+
+
+                menuRate.appendChild(star)
+
+            }
+            const numRev = document.createElement('span')
+            numRev.classList.add('total-rev')
+
+            numRev.textContent = menuOptions.items[i]['reviews']
+            menuRate.appendChild(numRev)
+            // create price n buy btn container
+            const priceContainer = document.createElement('div')
+            priceContainer.classList.add('price-buy-btn-container')
+
+                // create price and btn
+                const price = document.createElement('div')
+                price.classList.add('menu-price')
+                price.textContent = menuOptions.items[i]['price']
+
+                const menuBuyBtn = document.createElement('div') 
+                menuBuyBtn.classList.add('menu-buy-btn')
+                menuBuyBtn.textContent = "Buy Now"
+        
+        //append elemen
+        menuImgContainer.appendChild(menuImg)
+        priceContainer.appendChild(price)
+        priceContainer.appendChild(menuBuyBtn)
+        
+        menu.appendChild(menuImgContainer)
+        menu.appendChild(menuName)
+        menu.appendChild(menuRate)
+        menu.appendChild(priceContainer)
+        
+
+
+        menuSelectionContainer.appendChild(menu)
+    }
+}
+
+addMenuToDOM()
